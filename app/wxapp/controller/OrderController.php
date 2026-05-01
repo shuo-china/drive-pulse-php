37414f706bc3dd15e8e92656da6d8fb17d4bbe5c
aaron <china.jinshuo@gmail.com>
2026-05-01 09:01:09 +0800

    daily

--- app/wxapp/controller/OrderController.php
+++ app/wxapp/controller/OrderController.php
@@ -9,7 +9,32 @@ class OrderController extends BaseController
 {
     public function pagination()
     {
-        $orders = Order::with(['user', 'target_user'])->paginate();
+        $param = $this->request->param();
+        $map = [];
+        if (!empty($param['channel_id'])) {
+            $map[] = ['channel_id', '=', $param['channel_id']];
+        }
+        if (!empty($param['user_id'])) {
+            $map[] = ['user_id', '=', $param['user_id']];
+        }
+        if (!empty($param['target_user_id'])) {
+            $map[] = ['target_user_id', '=', $param['target_user_id']];
+        }
+        if (!empty($param['nickname'])) {
+            $map[] = ['user.nickname', 'like', '%' . $param['nickname'] . '%'];
+        }
+        if (!empty($param['uid'])) {
+            $map[] = ['user.uid', '=', $param['uid']];
+        }
+        if (!empty($param['date'])) {
+            $startTime = strtotime($param['date'] . ' 00:00:00');
+            if ($startTime !== false) {
+                $endTime = $startTime + 86399;
+                $map[] = ['create_time', 'between', [$startTime, $endTime]];
+            }
+        }
+ 
+        $orders = Order::with(['user', 'target_user'])->where($map)->paginate();
         $this->success(200, $orders);
     }
 
