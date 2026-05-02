11a5dd15e51e54b4eb1b3e59f0f9bcc2b35cb6db
aaron <china.jinshuo@gmail.com>
2026-05-02 14:08:12 +0800

    daily

--- app/wxapp/model/Notice.php
+++ app/wxapp/model/Notice.php
@@ -4,5 +4,8 @@ namespace app\wxapp\model;
 
 class Notice extends BaseModel
 {
-
+    public function getCreateTimeAttr($value)
+    {
+        return $value ? date('Y-m-d H:i', $value) : '';
+    }
 }
